12672112d5e6b9318900cb25d63da19a61cf6621
ValentinTsonevski <tsonevski99@gmail.com>
2023-08-28 00:01:24 +0300

    Login feature added

--- src/main/java/com/example/motogear/MotoGearApplication.java
+++ src/main/java/com/example/motogear/MotoGearApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MotoGearApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MotoGearApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MotoGearApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/motogear/MotoGearApplicationTests.java
+++ src/test/java/com/example/motogear/MotoGearApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MotoGearApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
